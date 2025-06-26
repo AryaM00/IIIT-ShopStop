866256a65c3362914c917be4c8f36683438af7c8
AryaM00 <aryamahendraker@gmail.com>
2025-06-26 16:35:17 +0530

    render backend

--- backend/config/casConfig.js
+++ backend/config/casConfig.js
@@ -1,6 +1,6 @@
 const casConfig = {
     cas_url: 'https://login.iiit.ac.in/cas',
-    service_url: 'http://localhost:5000', 
+    service_url: process.env.BACKEND_URL || 'http://localhost:5000', 
     cas_version: '3.0'
 };
 
